3fe1150ab3504053e3f61bd35ff890a98d044b12
svkreml <svkreml@gmail.com>
2018-06-30 13:22:29 +0300

    fix

--- JavaFxGui/src/main/java/JavaFxUtils.java
+++ JavaFxGui/src/main/java/JavaFxUtils.java
@@ -4,6 +4,7 @@ import java.io.File;
 
 public class JavaFxUtils {
     final public static int MAX_FILE_SIZE_FOR_TEXT_AREA = 1_000_000;
+
     public static File fileChooser(String s) {
         FileChooser fileChooser = new FileChooser();
         File selectedFile = fileChooser.showOpenDialog(null);
